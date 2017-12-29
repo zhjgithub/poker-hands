b0dfaa853c2c7f94897d3fbd5de3c39640120b0e
organism <neuqzhj@live.com>
2017-12-29 17:30:35 +0800

    feature: Add card ranks tests

--- poker.py
+++ poker.py
@@ -79,6 +79,10 @@ def test():
     fk = "9D 9H 9S 9C 7D".split()  # four of a kind
     fh = "TD TC TH 7C 7D".split()  # full house
 
+    assert card_ranks(sf) == [10, 9, 8, 7, 6]
+    assert card_ranks(fk) == [9, 9, 9, 9, 7]
+    assert card_ranks(fh) == [10, 10, 10, 7, 7]
+
     assert poker([sf, fk, fh]) == sf
     assert poker([fk, fh]) == fk
     assert poker([fh, fh]) == fh
