479cb4587e24c468728fa54563e29aed9e946054
organism <neuqzhj@live.com>
2017-12-29 17:03:42 +0800

    feature: Add hand rank tests

--- poker.py
+++ poker.py
@@ -64,6 +64,10 @@ def test():
     assert poker([fh]) == fh
     assert poker([sf] + 99 * [fh]) == sf
 
+    assert hand_rank(sf) == (8, 10)
+    assert hand_rank(fk) == (7, 9, 7)
+    assert hand_rank(fh) == (6, 10, 7)
+
     return 'tests pass'
 
 
