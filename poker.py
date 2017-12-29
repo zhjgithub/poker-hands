c2dc755df49242b1a45dc29b4f49eb11b5a6c016
organism <neuqzhj@live.com>
2017-12-29 14:44:55 +0800

    feature: Add tests

--- poker.py
+++ poker.py
@@ -16,3 +16,21 @@ def hand_rank(hand):
     Return hand rank
     '''
     return None
+
+
+def test():
+    '''
+    Test cases for the function in poker program
+    '''
+    sf = "6C 7C 8C 9C TC".split()  # straight flush
+    fk = "9D 9H 9S 9C 7D".split()  # four of a kind
+    fh = "TD TC TH 7C 7D".split()  # full house
+
+    assert poker([sf, fk, fh]) == sf
+    assert poker([fk, fh]) == fk
+    assert poker([fh, fh]) == fh
+
+    return 'tests pass'
+
+
+print test()
