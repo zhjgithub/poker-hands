17f2ce1456cd050c90f8594658f6aa21f9a80ba0
organism <neuqzhj@live.com>
2017-12-29 16:56:27 +0800

    feature: Implement part of hand rank

--- poker.py
+++ poker.py
@@ -13,9 +13,41 @@ def poker(hands):
 
 def hand_rank(hand):
     '''
-    Return hand rank
+    Return a value indicating the ranking of a hand.
     '''
-    return None
+    ranks = card_ranks(hand)
+    if straight(ranks) and flush(hand):
+        return (8, max(ranks))
+    elif kind(4, ranks):
+        return (7, kind(4, ranks), kind(1, ranks))
+
+
+def card_ranks(hand):
+    '''
+    Return card ranks.
+    '''
+    return []
+
+
+def straight(ranks):
+    '''
+    Return true if ranks are straight.
+    '''
+    return True
+
+
+def flush(hand):
+    '''
+    Return true if hand is flush.
+    '''
+    return True
+
+
+def kind(type, ranks):
+    '''
+    Return type of a kind number.
+    '''
+    return 0
 
 
 def test():
