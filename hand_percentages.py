4a602112b290bbbec63feb52749138bd162e83fd
organism <neuqzhj@live.com>
2017-12-30 21:14:54 +0800

    feature: Add hand type probability calculate function

---
+++ hand_percentages.py
@@ -0,0 +1,18 @@
+import poker
+
+
+def hand_percentages(n=700 * 1000):
+    '''
+    Sample n random hands and print a table of percentages for each type of hand.
+    It could be run very long time, so decrease the parameter.
+    '''
+    counts = [0] * 9
+    for i in range(n // 10):
+        for hand in poker.deal(10):
+            ranking = poker.hand_rank(hand)[0]
+            counts[ranking] += 1
+    for i in reversed(range(9)):
+        print('%d: %6.3f %%' % (i, 100.0 * counts[i] / n))
+
+
+# hand_percentages()
