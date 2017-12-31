6dabb9333e7e6529e524f9a40cbde86fd2377aa6
organism <neuqzhj@live.com>
2017-12-31 16:45:19 +0800

    feature: Add best hand of n-card hand function

--- poker.py
+++ poker.py
@@ -3,6 +3,7 @@
 Poker hands
 '''
 
+import itertools
 from shuffle import shuffle
 
 mydeck = [r + s for r in '23456789TJQKA' for s in 'SHDC']
@@ -14,6 +15,11 @@ def deal(numhands, n=5, deck=mydeck):
     '''
     shuffle(deck)
     return [deck[n * i:n * (i + 1)] for i in range(numhands)]
+
+
+def best_hand(hand):
+    "From a n-card hand, return the best 5 card hand."
+    return max(itertools.combinations(hand, 5), key=hand_rank)
 
 
 def poker(hands):
@@ -105,5 +111,22 @@ def test():
     return 'tests pass'
 
 
+def test_best_hand():
+    '''
+    Test best 5-card from a 7-card.
+    '''
+    assert (sorted(best_hand("6C 7C 8C 9C TC 5C JS".split())) == [
+        '6C', '7C', '8C', '9C', 'TC'
+    ])
+    assert (sorted(best_hand("TD TC TH 7C 7D 8C 8S".split())) == [
+        '8C', '8S', 'TC', 'TD', 'TH'
+    ])
+    assert (sorted(best_hand("JD TC TH 7C 7D 7S 7H".split())) == [
+        '7C', '7D', '7H', '7S', 'JD'
+    ])
+    print('test_best_hand passes')
+
+
 if __name__ == '__main__':
     print(test())
+    test_best_hand()
